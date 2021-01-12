e771c246aadd47f9519409530857b9272efb4105
ricardo <ricardotvera.54@gmail.com>
2021-01-12 14:44:07 -0800

    implemented app js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <BookList />
+    <BooksForm />
+  </div>
+);
 
 export default App;
