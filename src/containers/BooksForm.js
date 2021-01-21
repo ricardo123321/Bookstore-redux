a1d0c0bb32a8418f87db200080178c4886bc874b
ricardo <ricardotvera.54@gmail.com>
2021-01-21 13:23:07 -0800

    Create BooksForm.js

---
+++ src/containers/BooksForm.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const bookCategories = [
+  "Action",
+  "Biography",
+  "History",
+  "Horror",
+  "Kids",
+  "Learning",
+  "Sci-Fi",
+];
+
+const BooksForm = () => (
+  <form>
+    <div className="input-group">
+      <input type="text" />
+    </div>
+    <div className="input-group">
+      <select name="book-categories" id="categories">
+        {bookCategories.map((category) => (
+          <option key={`key-${category}`} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
+    </div>
+    <button type="button">Submit</button>
+  </form>
+);
+
+export default BooksForm;
