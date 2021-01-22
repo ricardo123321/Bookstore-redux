0553a18b449733fca39a32e6315cf6161c163fc3
ricardo <ricardotvera.54@gmail.com>
2021-01-22 14:41:18 -0800

    added remove button

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 const Book = ({ bookID, title, category }) => (
   <tr>
     <td>{bookID}</td>
     <td>{title}</td>
     <td>{category}</td>
+    <td>
+      <button
+        onClick={() => {
+          const book = { bookID, title, category };
+          handleRemoveBook(book);
+        }}
+        type="button"
+      >
+        Remove
+      </button>
+    </td>
   </tr>
 );
 
