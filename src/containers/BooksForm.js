055f8c96e9dfed580f60c03036d6f0b6d1fa6df6
ricardo <ricardotvera.54@gmail.com>
2021-01-22 11:49:32 -0800

    stored title and category

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,31 +1,37 @@
-import React from 'react';
+import React from "react";
+import { createBook } from "../actions/index";
 
 const bookCategories = [
-  'Action',
-  'Biography',
-  'History',
-  'Horror',
-  'Kids',
-  'Learning',
-  'Sci-Fi',
+  "Action",
+  "Biography",
+  "History",
+  "Horror",
+  "Kids",
+  "Learning",
+  "Sci-Fi",
 ];
 
-const BooksForm = () => (
-  <form>
-    <div className="input-group">
-      <input type="text" />
-    </div>
-    <div className="input-group">
-      <select name="book-categories" id="categories">
-        {bookCategories.map(category => (
-          <option key={`key-${category}`} value={category}>
-            {category}
-          </option>
-        ))}
-      </select>
-    </div>
-    <button type="button">Submit</button>
-  </form>
-);
+const BooksForm = ({ createBook }) => {
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+
+  return (
+    <form>
+      <div className="input-group">
+        <input type="text" />
+      </div>
+      <div className="input-group">
+        <select name="book-categories" id="categories">
+          {bookCategories.map((category) => (
+            <option key={`key-${category}`} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
+      <button type="button">Submit</button>
+    </form>
+  );
+};
 
 export default BooksForm;
