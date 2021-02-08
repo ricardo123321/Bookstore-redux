6032bd4e0b34b9b36be63578b4af52a6bc3f4c63
ricardo <ricardotvera.54@gmail.com>
2021-02-07 17:28:37 -0800

    Update CategoryFilter.js

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,2 +1,17 @@
 import React, { useState } from "react";
-import PropTypes from "prop-types";
+import { bookCategories } from "../helpers/index";
+
+const CategoryFilter = () => {
+  return (
+    <select>
+      <option value="All">All</option>
+      {bookCategories.map((book) => (
+        <option key={book} value={book}>
+          {book}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default CategoryFilter;
